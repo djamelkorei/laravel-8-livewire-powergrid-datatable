8c3574992e821dd48b803ad7ef29d829dcdf2b7f
djamel-kr <djamel.korei@gmail.com>
2021-11-17 01:10:48 +0100

    update app

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word(),
+            'price' => $this->faker->randomNumber(2),
+            'active' => $this->faker->boolean()
+        ];
+    }
+}
